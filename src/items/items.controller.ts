4201877fb82226797c9ee4f2fa6a51eba971fcc6
Seth Lewandowski <sethlew@protonmail.com>
2021-05-09 07:13:02 -0700

    added delete and put routes :

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -17,12 +17,22 @@ export class ItemsController {
   }
 
   @Get(':id')
-  findOne(@Param() param): string {
-    return `Item: ${param.id}`;
+  findOne(@Param('id') id): string {
+    return `Item: ${id}`;
   }
 
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update ${id} - Name: ${updateItemDto.name}`;
   }
 }
